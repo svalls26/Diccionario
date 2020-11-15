57bff50ed67226480312cb8e705327252aa132ff
svalls26 <74300901+svalls26@users.noreply.github.com>
2020-11-15 17:04:32 +0100

    Update examen.py

--- src/examen/examen.py
+++ src/examen/examen.py
@@ -1,44 +1,50 @@
 import sys 
 import pandas as pd
 import logging
+import os 
 
 class nombreArchivos:
 
-    def __init__(self, enviroments, logger, dataframe=None):
+    def __init__(self, enviroments, logger, name_files):
         """
         This is the class constructor
         """
-        self.__file = enviroments.file_name
+        self.__nameFiles = name_files
         self.__path = enviroments.relative_path
-        self.__output = enviroments.output_file
+        self.__output = enviroments.output_name
         self.logger = logger
-        self.dataframe = dataframe
     
-    def read_data(self):
-        """Read the csv and create a dataframe with those data"""
-        try: 
-            self.logger.logger.log(logging.INFO, "Reading CSV File using Pandas")
-            self.dataframe = pd.read_csv(self.__path+self.__file, sep=";")
-            print(self.dataframe.dtypes)
-            self.number_rows = len(self.dataframe.index)
-            #print(self.number_rows)
-            
-            
-        
+    def SaveNames(self):
+        try:
+            with open(self.__path+self.__output, 'w') as fichero:
+                for name in self.__nameFiles:
+                    fichero.write('{0}\n'.format(name))
+                    #print(self.number_rows)
+    
         except Exception as err:
-            self.logger.logger.log(logging.ERROR, "Error read_data function ({}".format(err) + ")")
+            self.logger.logger.log(logging.ERROR, "Error SaveNames function ({}".format(err) + ")")
             sys.exit(-1)
 
-    def FilterVehiculo(self, letter):
+    def SearchNames(self):
         """Filter evehicles by type"""
-        try: 
-            self.logger.logger.log(logging.INFO, "FilterVehicle function") 
-            self.df_letter = self.dataframe[self.dataframe['Tipo_Vehiculo'] == letter]
+        try:           
+            for root, dirs, files in os.walk(".", topdown=False):
+                
+                for name in files:
+                    text = name.split('.')
+                    print(text)
+                    if len(text)>=2:
+                        if text[1] == "txt":
+                            self.__nameFiles.append(name)
+                        
+            print(self.__nameFiles)
+            self.__nameFiles.remove('requirements.txt')
+            #self.__nameFiles.remove('file_names.txt')
 
-            return self.df_letter
+            return self.__nameFiles
         
         except Exception as err:
-            self.logger.logger.log(logging.ERROR, "Error FilterVehicle function ({}".format(err) + ")")
+            self.logger.logger.log(logging.ERROR, "Error SearchNames function ({}".format(err) + ")")
             sys.exit(-1)
             
             
