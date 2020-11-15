ca976d72920ea1d1bf4f254abcc9a877345af4a2
svalls26 <74300901+svalls26@users.noreply.github.com>
2020-11-15 17:04:34 +0100

    Update funcionesSara.py

--- src/funcionesSara.py
+++ src/funcionesSara.py
@@ -1,2 +1,21 @@
-# -*- coding: utf-8 -*-
+import os
 
+
+def save_names(name_files):
+    with open('name_files.txt', 'w') as fichero:
+        for name in name_files:
+            fichero.write('{0}\n'.format(name))
+
+def search_names(name_files):
+    
+    for root, dirs, files in os.walk(".", topdown=False):
+        
+        for name in files:
+            text = name.split('.')
+            print(name)
+            if 'txt' in name:
+                name_files.append(name)
+
+    return name_files
+
+        
